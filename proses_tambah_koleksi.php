5342b6fbbacf45a6f606d6a31086c1c042e996f0
akmsy <akmsyyy1@gmail.com>
2026-05-14 10:04:30 +0700

    ubah method POST

--- proses_tambah_koleksi.php
+++ proses_tambah_koleksi.php
@@ -1,11 +1,11 @@
 <?php 
     include 'koneksi.php';
 
-    $kodeBuku = $_GET['kode_buku'];
-    $judulBuku = $_GET['judul'];
-    $pengarangBuku = $_GET['pengarang'];
-    $kategoriBuku = $_GET['kategori'];
-    $stokBuku = $_GET['stok_buku'];
+    $kodeBuku = $_POST['kode_buku'];
+    $judulBuku = $_POST['judul'];
+    $pengarangBuku = $_POST['pengarang'];
+    $kategoriBuku = $_POST['kategori'];
+    $stokBuku = $_POST['stok_buku'];
 
     $query = "INSERT INTO koleksi_buku (kode_buku, judul, pengarang, kategori, stok) VALUES ('$kodeBuku', '$judulBuku', '$pengarangBuku', '$kategoriBuku', $stokBuku)";
     $result = mysqli_query($koneksi, $query);
